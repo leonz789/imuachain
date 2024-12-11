d9d7e11fe6d0aa3c8905ff4a6e8e8bb6b979543f
leonz789 <zleon789@gmail.com>
2024-12-11 21:19:33 +0800

    fix(oracle): restart on validator set change

--- x/oracle/keeper/aggregator/context.go
+++ x/oracle/keeper/aggregator/context.go
@@ -252,10 +252,10 @@ func (agc *AggregatorContext) SealRound(ctx sdk.Context, force bool) (success []
 					}
 					// TODO: optimize operformance
 					sealed = append(sealed, feederID)
-					if !windowClosedMap[feederID] {
-						// this should be clear after performanceReview
-						agc.RemoveWorker(feederID)
-					}
+					//					if !windowClosedMap[feederID] {
+					//						// this should be clear after performanceReview
+					//						agc.RemoveWorker(feederID)
+					//					}
 				}
 			default:
 				ctx.Logger().Info("mode other than 1 is not support now")
@@ -271,7 +271,7 @@ func (agc *AggregatorContext) SealRound(ctx sdk.Context, force bool) (success []
 
 // PrepareEndBlock is called at EndBlock stage, to prepare the roundInfo for the next block(of input block)
 // func (agc *AggregatorContext) PrepareRoundEndBlock(ctx sdk.Context, block uint64) {
-func (agc *AggregatorContext) PrepareRoundEndBlock(block uint64) (newRoundFeederIDs []uint64) {
+func (agc *AggregatorContext) PrepareRoundEndBlock(block uint64, forceSealed bool) (newRoundFeederIDs []uint64) {
 	if block < 1 {
 		return newRoundFeederIDs
 	}
@@ -303,6 +303,9 @@ func (agc *AggregatorContext) PrepareRoundEndBlock(block uint64) (newRoundFeeder
 				round.status = roundStatusClosed
 			} else {
 				round.status = roundStatusOpen
+				if forceSealed {
+					round.status = roundStatusClosed
+				}
 				if left == 0 {
 					// set nonce for corresponding feederID for new roud start
 					newRoundFeederIDs = append(newRoundFeederIDs, feederIDUint64)
