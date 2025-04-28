501502bbec8d512d84b1e52fea2b7343b496739a
leonz789 <zleon789@gmail.com>
2025-04-28 09:55:48 +0800

    delay version change on deposit update proto to add accumulated deposit amount for version add new version type with two kinds of 'version'

--- x/oracle/types/native_token.go
+++ x/oracle/types/native_token.go
@@ -8,3 +8,24 @@ func (b *Balances) Append(bi *BalanceInfo) {
 	}
 	b.BalanceList = append(b.BalanceList, bi)
 }
+
+func NewStakerInfo(stakerAddr, validatorPubkey string, version uint64) *StakerInfo {
+	return &StakerInfo{
+		StakerAddr:  stakerAddr,
+		StakerIndex: 0,
+		ValidatorPubkeyList: []*ValidatorVersion{
+			{
+				ValidatorPubkey: validatorPubkey,
+				Version:         version,
+			},
+		},
+		BalanceList: make([]*BalanceInfo, 0, 1),
+	}
+}
+
+func (s *StakerInfo) Append(b *BalanceInfo) {
+	if len(s.BalanceList) >= maxSize {
+		s.BalanceList = s.BalanceList[len(s.BalanceList)-maxSize:]
+	}
+	s.BalanceList = append(s.BalanceList, b)
+}
